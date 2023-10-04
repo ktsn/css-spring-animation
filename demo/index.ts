9c56a0cfde04d0e784face6c22fc0f4550a5d937
ktsn <ktsn@hey.com>
2023-10-04 10:27:15 +0900

    init

---
+++ demo/index.ts
@@ -0,0 +1,3 @@
+import { test } from '../src/main'
+
+test()
---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'CSSSpringAnimation',
+      fileName: 'css-spring-animation'
+    },
+  },
+})
