f5d33bba011722f0bac83fb9a959651422619eb9
ktsn <ktsn@hey.com>
2023-10-06 13:38:01 +0900

    vizualize spring graph

--- demo/visualizer/App.vue
+++ demo/visualizer/App.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
-import { ref, watchEffect } from 'vue'
+import { ref, shallowRef, watchEffect } from 'vue'
+import { calcSpringValue } from '../../src/main'
 import { useSpringStyle } from '../../src/vue/main'
+
+const from = 0
+const to = 300
 
 const parameters = ref({
   velocity: 0,
@@ -8,7 +12,8 @@ const parameters = ref({
   duration: 1000,
 })
 
-const x = ref(0)
+const x = ref(from)
+const canvas = shallowRef<HTMLCanvasElement>()
 
 const style = useSpringStyle(
   x,
@@ -20,27 +25,78 @@ const style = useSpringStyle(
   () => {
     return {
       ...parameters.value,
-      disabled: x.value === 0,
+      disabled: x.value === from,
     }
   },
 )
 
-let timer: number | undefined
-watchEffect(() => {
-  window.clearInterval(timer)
+function render(): void {
+  const ctx = canvas.value?.getContext('2d')
+  if (!ctx) {
+    return
+  }
 
-  timer = window.setInterval(() => {
-    x.value = 0
+  const w = 400
+  const h = 200
+
+  ctx.clearRect(0, 0, w, h)
+
+  ctx.beginPath()
+  ctx.moveTo(0, h)
+
+  for (let i = 0; i < w; i++) {
+    const t = i / w
+    const value = calcSpringValue({
+      time: t,
+      from,
+      to,
+      initialVelocity: parameters.value.velocity,
+      ...parameters.value,
+    })
+
+    const y = (2 - value / (to - from)) * (h / 2)
+    ctx.lineTo(i, y)
+  }
+
+  ctx.stroke()
+}
+
+let intervalTimer: number | undefined
+let rafTimer: number | undefined
+watchEffect(() => {
+  window.clearInterval(intervalTimer)
+  if (rafTimer !== undefined) {
+    window.cancelAnimationFrame(rafTimer)
+  }
+
+  intervalTimer = window.setInterval(() => {
+    x.value = from
     requestAnimationFrame(() => {
-      x.value = 300
+      x.value = to
     })
   }, parameters.value.duration + 1000)
+})
+
+requestAnimationFrame(function loop() {
+  render()
+  requestAnimationFrame(loop)
 })
 </script>
 
 <template>
   <div class="preview">
-    <div class="preview-ball" :style="style"></div>
+    <div class="preview-behavior">
+      <div class="preview-ball" :style="style"></div>
+    </div>
+
+    <div class="preview-graph">
+      <canvas
+        ref="canvas"
+        class="preview-graph-canvas"
+        width="400"
+        height="200"
+      ></canvas>
+    </div>
   </div>
 
   <div class="parameters">
@@ -93,10 +149,17 @@ body {
 
 <style scoped>
 .preview {
+  display: flex;
+  gap: 20px;
+}
+
+.preview-behavior {
+  display: flex;
+  align-items: center;
   overflow: hidden;
-  padding: 50px;
+  padding: 25px;
   max-width: 100%;
-  width: 450px;
+  width: 400px;
   background-color: #fff;
 }
 
@@ -105,5 +168,11 @@ body {
   height: 50px;
   border-radius: 50%;
   background-color: #259902;
+}
+
+.preview-graph {
+  display: flex;
+  width: 400px;
+  background-color: #fff;
 }
 </style>
