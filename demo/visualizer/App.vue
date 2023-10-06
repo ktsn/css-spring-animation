ac0195089c50d7b7cbd4c349ffd3509c8297c7a5
ktsn <ktsn@hey.com>
2023-10-06 20:43:06 +0900

    improve visualizer

--- demo/visualizer/App.vue
+++ demo/visualizer/App.vue
@@ -4,7 +4,10 @@ import { calcSpringValue } from '../../src/main'
 import { useSpringStyle } from '../../src/vue/main'
 
 const from = 0
-const to = 300
+const to = 350
+
+const canvasWidth = 500
+const canvasHeight = 300
 
 const parameters = ref({
   velocity: 0,
@@ -30,19 +33,48 @@ const style = useSpringStyle(
   },
 )
 
-function render(): void {
+function render(time: number): void {
   const ctx = canvas.value?.getContext('2d')
   if (!ctx) {
     return
   }
 
-  const w = 400
-  const h = 200
+  const w = canvasWidth
+  const h = canvasHeight
+  const p = 50
 
   ctx.clearRect(0, 0, w, h)
 
+  ctx.lineWidth = 1
+  ctx.setLineDash([2, 4])
+  ctx.strokeStyle = '#aaa'
+
   ctx.beginPath()
-  ctx.moveTo(0, h)
+  ctx.moveTo(0, p)
+  ctx.lineTo(w, p)
+  ctx.stroke()
+
+  ctx.beginPath()
+  ctx.moveTo(0, h - p)
+  ctx.lineTo(w, h - p)
+  ctx.stroke()
+
+  ctx.beginPath()
+  ctx.moveTo(0, h / 2)
+  ctx.lineTo(w, h / 2)
+  ctx.stroke()
+
+  ctx.beginPath()
+  ctx.moveTo(w / 2, 0)
+  ctx.lineTo(w / 2, h)
+  ctx.stroke()
+
+  ctx.lineWidth = 2
+  ctx.setLineDash([])
+  ctx.strokeStyle = '#000'
+
+  ctx.beginPath()
+  ctx.moveTo(0, h - p)
 
   for (let i = 0; i < w * 2; i++) {
     const t = i / w
@@ -54,10 +86,17 @@ function render(): void {
       ...parameters.value,
     })
 
-    const y = (2 - value / (to - from)) * (h / 2)
+    const y = (2 - value / (to - from)) * (h / 2 - p) + p
     ctx.lineTo(i / 2, y)
   }
+  ctx.stroke()
 
+  ctx.lineWidth = 1
+  ctx.strokeStyle = '#00f'
+
+  ctx.beginPath()
+  ctx.moveTo((time * w) / 2, 0)
+  ctx.lineTo((time * w) / 2, h)
   ctx.stroke()
 }
 
@@ -65,72 +104,107 @@ let intervalTimer: number | undefined
 let rafTimer: number | undefined
 watchEffect(() => {
   window.clearInterval(intervalTimer)
-  if (rafTimer !== undefined) {
-    window.cancelAnimationFrame(rafTimer)
-  }
 
-  intervalTimer = window.setInterval(() => {
+  function start(): void {
+    if (rafTimer !== undefined) {
+      window.cancelAnimationFrame(rafTimer)
+    }
+
     x.value = from
     requestAnimationFrame(() => {
       x.value = to
     })
-  }, parameters.value.duration + 1000)
-})
 
-requestAnimationFrame(function loop() {
-  render()
-  requestAnimationFrame(loop)
+    let start: number | undefined
+
+    function loop(now: number): void {
+      if (start === undefined) {
+        start = now
+      }
+      render((now - start) / parameters.value.duration)
+      rafTimer = requestAnimationFrame(loop)
+    }
+
+    rafTimer = requestAnimationFrame(loop)
+  }
+
+  start()
+
+  intervalTimer = window.setInterval(start, parameters.value.duration * 2)
 })
 </script>
 
 <template>
-  <div class="preview">
-    <div class="preview-behavior">
-      <div class="preview-ball" :style="style"></div>
+  <div class="wrapper">
+    <div class="preview">
+      <div class="preview-behavior">
+        <div class="preview-ball" :style="style"></div>
+      </div>
+
+      <div class="preview-graph">
+        <canvas
+          ref="canvas"
+          class="preview-graph-canvas"
+          :width="canvasWidth"
+          :height="canvasHeight"
+        ></canvas>
+      </div>
     </div>
 
-    <div class="preview-graph">
-      <canvas
-        ref="canvas"
-        class="preview-graph-canvas"
-        width="400"
-        height="200"
-      ></canvas>
-    </div>
-  </div>
+    <div class="parameters">
+      <div class="parameter">
+        <label>Velocity</label>
+        <input
+          type="range"
+          min="-10000"
+          max="10000"
+          step="10"
+          v-model.number="parameters.velocity"
+        />
+        <input
+          type="number"
+          min="-10000"
+          max="10000"
+          step="100"
+          v-model.number="parameters.velocity"
+        />
+      </div>
 
-  <div class="parameters">
-    <div class="parameter">
-      <label>Velocity</label>
-      <input
-        type="number"
-        min="-10000"
-        max="10000"
-        step="100"
-        v-model.number="parameters.velocity"
-      />
-    </div>
+      <div class="parameter">
+        <label>Bounce</label>
+        <input
+          type="range"
+          min="-1"
+          max="1"
+          step="0.01"
+          v-model.number="parameters.bounce"
+        />
+        <input
+          type="number"
+          min="-1"
+          max="1"
+          step="0.1"
+          v-model.number="parameters.bounce"
+        />
+      </div>
 
-    <div class="parameter">
-      <label>Bounce</label>
-      <input
-        type="number"
-        min="-1"
-        max="1"
-        step="0.1"
-        v-model.number="parameters.bounce"
-      />
-    </div>
-
-    <div class="parameter">
-      <label>Duration</label>
-      <input
-        type="number"
-        min="100"
-        max="5000"
-        step="100"
-        v-model.number="parameters.duration"
-      />
+      <div class="parameter">
+        <label>Duration</label>
+        <input
+          type="range"
+          min="100"
+          max="5000"
+          step="10"
+          v-model.number="parameters.duration"
+        />
+        <input
+          type="number"
+          min="100"
+          max="5000"
+          step="100"
+          v-model.number="parameters.duration"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -143,11 +217,18 @@ requestAnimationFrame(function loop() {
 }
 
 body {
+  margin: 0;
   background-color: #e0e0e0;
 }
 </style>
 
 <style scoped>
+.wrapper {
+  margin: auto;
+  padding: 20px;
+  width: 1060px;
+}
+
 .preview {
   display: flex;
   gap: 20px;
@@ -157,9 +238,8 @@ body {
   display: flex;
   align-items: center;
   overflow: hidden;
-  padding: 25px;
-  max-width: 100%;
-  width: 400px;
+  flex: 1 1 0;
+  padding: 25px 50px;
   background-color: #fff;
 }
 
@@ -172,7 +252,8 @@ body {
 
 .preview-graph {
   display: flex;
-  width: 400px;
+  flex: 1 1 0;
+  width: 500px;
   background-color: #fff;
 }
 </style>
