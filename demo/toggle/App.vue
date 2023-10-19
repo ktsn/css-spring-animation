c0232ab817dd3bf9a8a2ebdd55a320dba3c82748
ktsn <ktsn@hey.com>
2023-10-19 09:19:26 +0900

    add toggle demo

---
+++ demo/toggle/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+import { spring } from '../../src/vue'
+
+const moved = ref(false)
+</script>
+
+<template>
+  <button type="button" class="button" @click="moved = !moved">Toggle</button>
+
+  <spring.div
+    class="rectangle"
+    :spring-style="{
+      translate: moved ? '100px' : '0px',
+    }"
+    :duration="600"
+    :bounce="0.2"
+  ></spring.div>
+</template>
+
+<style scoped>
+.button {
+  margin-bottom: 20px;
+}
+
+.rectangle {
+  width: 100px;
+  height: 100px;
+  border-radius: 6px;
+  background-color: red;
+}
+</style>
