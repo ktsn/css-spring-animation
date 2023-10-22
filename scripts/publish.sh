03b213628a028c6b1958858b2958792f57ea2d99
ktsn <ktsn@hey.com>
2023-10-22 18:06:13 +0900

    provide .js file as copy of .cjs

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,5 +4,7 @@ pnpm run typecheck --run
 pnpm run test --run
 pnpm run dts
 pnpm run -r build
+cp packages/core/dist/css-spring-animation-core.umd.cjs packages/core/dist/css-spring-animation-core.umd.js
+cp packages/vue/dist/css-spring-animation-vue.umd.cjs packages/vue/dist/css-spring-animation-vue.umd.js
 
-pnpm -r publish --access public
+#pnpm -r publish --access public
