6facc0d79b47c2031dc599a51872b9df8eaf0c5d
ktsn <ktsn@hey.com>
2023-12-29 16:35:55 +0900

    chore: update release script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,6 +5,11 @@ if [ -z $1 ]; then
   exit 1
 fi
 
+if [ -n "$(git status --porcelain)" ]; then
+  echo "Working directory is not clean" 1>&2
+  exit 1
+fi
+
 pnpm run typecheck --run
 pnpm run test --run
 pnpm run dts
@@ -12,5 +17,15 @@ pnpm run -r build
 cp packages/core/dist/css-spring-animation-core.umd.cjs packages/core/dist/css-spring-animation-core.umd.js
 cp packages/vue/dist/css-spring-animation-vue.umd.cjs packages/vue/dist/css-spring-animation-vue.umd.js
 
-npm version $1
+(cd packages/core; npm version $1 --git-tag-version false)
+(cd packages/vue; npm version $1 --git-tag-version false)
+
+cp README.md packages/vue/README.md
+cp README.ja.md packages/vue/README.ja.md
+conventional-changelog -p angular -i CHANGELOG.md -s
+
+git add .
+git commit -m "v$1"
+git tag v$1
+
 pnpm -r publish --access public
