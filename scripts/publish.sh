e3a22a2319173166c3c95730409829fb7ea8206f
ktsn <ktsn@hey.com>
2023-12-29 16:19:34 +0900

    chore: setup conventional changelog

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ -z $1 ]; then
+  echo "Please provide a version number" 1>&2
+  exit 1
+fi
+
 pnpm run typecheck --run
 pnpm run test --run
 pnpm run dts
@@ -7,4 +12,5 @@ pnpm run -r build
 cp packages/core/dist/css-spring-animation-core.umd.cjs packages/core/dist/css-spring-animation-core.umd.js
 cp packages/vue/dist/css-spring-animation-vue.umd.cjs packages/vue/dist/css-spring-animation-vue.umd.js
 
+npm version $1
 pnpm -r publish --access public
